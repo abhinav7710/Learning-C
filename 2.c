43f0dc108d37dbbab0dffab426c5f770878fd7ad
abhinav7710 <abhinav2000id@gmail.com>
2026-08-18 00:26:29 +0530

    Taking two integers as input and adding them

---
+++ 2.c
@@ -0,0 +1,13 @@
+//Taking two integers as input and adding them
+#include <stdio.h>
+int main() {
+    int a;
+    int b;
+    int sum;
+    printf("enter a number: ");
+    scanf("%d", &a);
+    printf("enter another number: ");
+    scanf("%d", &b);
+    printf("sum is: %d", sum = a + b);
+    return 0;
+}
